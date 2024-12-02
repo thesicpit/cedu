bc553dde42c9edf5b48b8e0883da2c62c3a50fc5
thesicpit <thesicpit@users.noreply.github.com>
2024-12-02 21:31:04 +1030

    Add files via upload

---
+++ Rename Computer.sh
@@ -0,0 +1,25 @@
+﻿#!/bin/bash
+
+userName=$(/bin/ls -la /dev/console | /usr/bin/cut -d " " -f 4)
+macModel=$(/usr/sbin/sysctl -n hw.model)
+
+setModel='STG-MBA'
+
+
+computerName="$userName-$setModel"
+
+# Set the ComputerName, HostName and LocalHostName
+/usr/sbin/scutil --set ComputerName "$computerName"
+/usr/sbin/scutil --set HostName "$computerName"
+/usr/sbin/scutil --set LocalHostName "$computerName"
+
+## Create dummy receipt to mark complete
+touch /Library/Receipts/com.stg.renameComplete.bom
+
+## Update Inventory
+/usr/local/bin/jamf setComputerName -name $computerName
+/usr/local/bin/jamf recon -endUsername $userName
+/usr/local/bin/jamf recon
+
+dscacheutil -flushcache
+q`qq`
